79c65c0e7965cc091ebe09fbeab8e4aed11ffe1d
Srikar092004 <167324238+Srikar092004@users.noreply.github.com>
2025-12-06 14:35:09 +0530

    Update queryKey to use uppercase 'TWEETS'

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -30,7 +30,7 @@ function Homepage() {
           `}
         >
           <TweetsBoard
-            queryKey={['tweets', {}]}
+            queryKey={['TWEETS', {}]}
             loading={status === 'loading'}
             pages={data?.pages || []}
             isFetching={isFetching}
